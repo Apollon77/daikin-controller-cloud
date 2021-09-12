7725cb6a608b1e64ea8fd65dde25f7b8ce45519f
Lars K.W. Gohlke <lkwg82@gmx.de>
2021-09-12 17:59:14 +0200

    lazy login and changed location for output

--- example/data_collector.js
+++ example/data_collector.js
@@ -2,13 +2,14 @@
  * Example Script to use the Daikin-Controller-Cloud library
  *
  * This example will use Username/Password from environment variable
- * DAIKIN_USERNAME and DAIKIN_PASSWORD and outputs data out to stderr
+ * DAIKIN_USERNAME and DAIKIN_PASSWORD and outputs data out to data.json
  * 
- * DAIKIN_USERNAME='x' DAIKIN_PASSWORD='y' node data_collector.js 2> data.json
+ * DAIKIN_USERNAME='x' DAIKIN_PASSWORD='y' node data_collector.js
  */
 
 const DaikinCloud = require('../index');
 const fs = require('fs');
+const path = require('path');
 
 
 async function main() {
@@ -19,23 +20,42 @@ async function main() {
     // console.log(`Cloud Device Details: ${JSON.stringify(daikinDeviceDetails)}`);
     // console.log(JSON.stringify(daikinDeviceDetails));
 
-    fs.writeFileSync('/dev/stderr', JSON.stringify(daikinDeviceDetails));
+    const file = process.env.OUTPUT_FILE || 'data.json'
+    fs.writeFileSync(file, JSON.stringify(daikinDeviceDetails));
 
     async function initDaikinCloud() {
-        const daikinCloud = new DaikinCloud();
+        let tokenSet;
+
+        // Load Tokens if they already exist on disk
+        const tokenFile = path.join(__dirname, 'tokenset.json');
+        if (fs.existsSync(tokenFile)) {
+            tokenSet = JSON.parse(fs.readFileSync(tokenFile).toString());
+        }
+
+        // Initialize Daikin Cloud Instance
+        const daikinCloud = new DaikinCloud(tokenSet);
+
+        // Event that will be triggered on new or updated tokens, save into file
+        daikinCloud.on('token_update', tokenSet => {
+            console.log(`UPDATED tokens, use for future and wrote to tokenset.json`);
+            fs.writeFileSync(tokenFile, JSON.stringify(tokenSet));
+        });
+
         const { username, password } = retrieveCredentials();
-        console.log("logging into ...");
-        await daikinCloud.login(username, password);
+        if (!fs.existsSync(tokenFile)) {
+            console.log("logging into ...");
+            await daikinCloud.login(username, password);
+        }
         return daikinCloud;
     }
 
     function retrieveCredentials() {
         const username = process.env.DAIKIN_USERNAME;
-        const password = process.env.DAIKIN_PASSWORD;
-
         if (username == undefined) {
             throw new Error("WARN please provide username as env variable 'DAIKIN_USERNAME'");
         }
+
+        const password = process.env.DAIKIN_PASSWORD;
         if (password == undefined) {
             throw new Error("WARN please provide password as env variable 'DAIKIN_PASSWORD'");
         }

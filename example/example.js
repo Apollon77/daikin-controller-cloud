a93e781ec0132b56a72c807c595e99c3a35a4494
Jeroen Van den Berghe <jeroen.van.den.berghe@persgroep.net>
2024-06-20 20:02:27 +0200

    - removed proxy (might re-add later but want to focus in the core functionality) - talk to official ONECTA Cloud API

--- example/example.js
+++ example/example.js
@@ -19,21 +19,6 @@ const fs = require('fs');
 const path = require('path');
 
 async function main() {
-    /**
-     * Options to initialize the DaikinCloud instance with
-     */
-    const options = {
-        logger: console.log,          // optional, logger function used to log details depending on loglevel
-        logLevel: 'info',             // optional, Loglevel of Library, default 'warn' (logs nothing by default)
-        proxyOwnIp: '192.168.xxx.xxx',// required, if proxy needed: provide own IP or hostname to later access the proxy
-        proxyPort: 8888,              // required: use this port for the proxy and point your client device to this port
-        proxyWebPort: 8889,           // required: use this port for the proxy web interface to get the certificate and start Link for login
-        proxyListenBind: '0.0.0.0',   // optional: set this to bind the proxy to a special IP, default is '0.0.0.0'
-        proxyDataDir: __dirname,      // Directory to store certificates and other proxy relevant data to
-        communicationTimeout: 10000,  // Amount of ms to wait for request and responses before timeout
-        communicationRetries: 3       // Amount of retries when connection timed out
-    };
-
     let tokenSet;
 
     // Load Tokens if they already exist on disk
@@ -43,7 +28,7 @@ async function main() {
     }
 
     // Initialize Daikin Cloud Instance
-    const daikinCloud = new DaikinCloud(tokenSet, options);
+    const daikinCloud = new DaikinCloud(process.env.ONECTA_CLIENT_ID, process.env.ONECTA_CLIENT_SECRET, tokenSet);
 
     // Event that will be triggered on new or updated tokens, save into file
     daikinCloud.on('token_update', tokenSet => {
@@ -51,28 +36,8 @@ async function main() {
         fs.writeFileSync(tokenFile, JSON.stringify(tokenSet));
     });
 
-    // If no tokens are existing start Proxy server process
-    if (! tokenSet) {
-        // start server
-        await daikinCloud.initProxyServer();
-        console.log(`Please visit http://${options.proxyOwnIp}:${options.proxyWebPort} and Login to Daikin Cloud please.`);
-        console.log();
-
-        // wait for user Login and getting the tokens
-        const resultTokenSet = await daikinCloud.waitForTokenFromProxy();
-        console.log(`Retrieved tokens, use for future: ${JSON.stringify(resultTokenSet)}`);
-
-        // stop Proxy server (and wait 1s before we do that to make sure
-        // the success page can be displayed correctly because waitForTokenFromProxy
-        // will resolve before the last request is sent to the browser!
-        await new Promise(resolve => setTimeout(resolve, 1000));
-        await daikinCloud.stopProxyServer();
-    }
-    // show some details about the tokens (could be outdated because first real request is done afterwards
-    console.log('Use Token with the following claims: ' + JSON.stringify(daikinCloud.getTokenSet().claims()));
-
     const daikinDeviceDetails = await daikinCloud.getCloudDeviceDetails();
-    // console.log(`Cloud Device Details: ${JSON.stringify(daikinDeviceDetails)}`);
+    console.log(`Cloud Device Details: ${JSON.stringify(daikinDeviceDetails)}`);
 
     const devices = await daikinCloud.getCloudDevices();
 

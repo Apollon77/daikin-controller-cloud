c08619359cac8cd67033dbe4d140c5f81e9fbf12
Ingo Fischer <github@fischer-ka.de>
2024-07-03 00:07:48 +0200

    Alpha1 Preparations (#140)
    * Tweak GHA
    
    * Update certs
    
    * Fix wrong export
    
    * Adjust client to
    
    * make answer template configurable
    * add redirect to local server and use as startpoint to get certificate accepted at the beginning and not failing at the end
    * make cert dir configurable
    
    * shell script executable rights
    
    * enhance example guidance
    
    * Add custom code provider
    
    .. to allow cases where e.g. a webserver is already opened or such
    
    * fix back
    
    * Adjustments from Review
    * allows to specify external address (ip/domain) and then redirect url is build automatically
    * allows to specify initial tokenSet in config - then no file is used
    * emits event on token update
    * splits cert path in two properties
    * camelCase the config
    
    * Camel case and real privates

--- src/onecta/oidc-client.ts
+++ src/onecta/oidc-client.ts
@@ -9,7 +9,6 @@ import {
     OnectaAPIBaseUrl,
     OnectaClientConfig,
     onecta_oidc_issuer,
-    onecta_oidc_auth_thank_you_html,
 } from './oidc-utils.js';
 
 import {
@@ -17,118 +16,123 @@ import {
 } from './oidc-callback-server.js';
 
 export class OnectaClient {
-
-    private _config: OnectaClientConfig;
-    private _client: BaseClient;
-    private _token_set: TokenSet | null;
-    private _emitter: EventEmitter;
-    private _get_token_set_queue: { resolve: (set: TokenSet) => any, reject: (err: Error) => any }[];
+    #config: OnectaClientConfig;
+    #client: BaseClient;
+    #tokenSet: TokenSet | null;
+    #emitter: EventEmitter;
+    #getTokenSetQueue: { resolve: (set: TokenSet) => any, reject: (err: Error) => any }[];
 
     constructor(config: OnectaClientConfig, emitter: EventEmitter) {
-        this._config = config;
-        this._emitter = emitter;
-        this._client = new onecta_oidc_issuer.Client({
-            client_id: config.oidc_client_id,
-            client_secret: config.oidc_client_secret,
+        this.#config = config;
+        this.#emitter = emitter;
+        this.#client = new onecta_oidc_issuer.Client({
+            client_id: config.oidcClientId,
+            client_secret: config.oidcClientSecret,
         });
-        this._token_set = null;
-        this._get_token_set_queue = [];
-    }
-
-    private async _authorize(): Promise<TokenSet> {
-        const { _config, _client } = this;
-        const redirect_uri = _config.oidc_callback_server_baseurl;
-        const req_state = randomBytes(32).toString('hex');
-        const auth_url = _client.authorizationUrl({
-            scope: OnectaOIDCScope.basic,
-            state: req_state,
-            redirect_uri,
-        });
-        this._emitter.emit('authorization_request', auth_url);
-        const auth_code = await startOnectaOIDCCallbackServer(this._config, req_state);
-        const token_set = await _client.grant({
-            grant_type: 'authorization_code',
-            client_id: _config.oidc_client_id,
-            client_secret: _config.oidc_client_secret,
-            code: auth_code,
-            redirect_uri,
-        });
-        return token_set;
-    }
-
-    private async _refresh(refresh_token: string): Promise<TokenSet> {
-        const { _client, _config } = this;
-        const token_set = await _client.grant({
-            grant_type: 'refresh_token',
-            client_id: _config.oidc_client_id,
-            client_secret: _config.oidc_client_secret,
-            refresh_token,
-        });
-        return token_set;
-    }
-
-    private async _loadTokenSet(): Promise<TokenSet | null> {
-        const { _config } = this;
-        try {
-            const data = await readFile(_config.oidc_tokenset_file_path, 'utf8');
-            const token_set = new TokenSet(JSON.parse(data));
-            return token_set;
-        } catch (err) {
-            if ((err as { code?: string }).code !== 'ENOENT') {
-                this._emitter.emit('error', 'Could not load OIDC tokenset from disk: ' + (err as Error).message);
-            }
-            return null;
+        this.#tokenSet = config.tokenSet ? new TokenSet(config.tokenSet) : null;
+        this.#getTokenSetQueue = [];
+        if (!config.oidcCallbackServerBaseUrl) {
+            config.oidcCallbackServerBaseUrl =
+                `https://${config.oidcCallbackServerExternalAddress ?? 
+                    config.oidcCallbackServerBindAddr}:${config.oidcCallbackServerPort}`;
         }
     }
 
-    private async _storeTokenSet(set: TokenSet): Promise<void> {
-        const { _config } = this;
-        try {
-            await writeFile(_config.oidc_tokenset_file_path, JSON.stringify(set, null, 2));
-        } catch (err) {
-            this._emitter.emit('error', 'Could not store OIDC tokenset to disk: ' + (err as Error).message);
+    async #authorize(): Promise<TokenSet> {
+        const redirectUri = this.#config.oidcCallbackServerBaseUrl;
+        const reqState = randomBytes(32).toString('hex');
+        const authUrl = this.#client.authorizationUrl({
+            scope: OnectaOIDCScope.basic,
+            state: reqState,
+            redirect_uri: redirectUri,
+        });
+        this.#emitter.emit('authorization_request', this.#config.oidcCallbackServerBaseUrl);
+        const authCode =
+            this.#config.customOidcCodeReceiver
+                ? await this.#config.customOidcCodeReceiver(authUrl, reqState)
+                : await startOnectaOIDCCallbackServer(this.#config, reqState, authUrl);
+        return await this.#client.grant({
+            grant_type: 'authorization_code',
+            client_id: this.#config.oidcClientId,
+            client_secret: this.#config.oidcClientSecret,
+            code: authCode,
+            redirect_uri: redirectUri,
+        });
+    }
+
+    async #refresh(refreshToken: string): Promise<TokenSet> {
+        return await this.#client.grant({
+            grant_type: 'refresh_token',
+            client_id: this.#config.oidcClientId,
+            client_secret: this.#config.oidcClientSecret,
+            refreshToken,
+        });
+    }
+
+    async #loadTokenSet(): Promise<TokenSet | null> {
+        if (this.#config.oidcTokenSetFilePath) {
+            try {
+                const data = await readFile(this.#config.oidcTokenSetFilePath, 'utf8');
+                return new TokenSet(JSON.parse(data));
+            } catch (err) {
+                if ((err as { code?: string }).code !== 'ENOENT') {
+                    this.#emitter.emit('error', 'Could not load OIDC tokenset from disk: ' + (err as Error).message);
+                }
+            }
+        }
+        return null;
+    }
+
+    async #storeTokenSet(set: TokenSet): Promise<void> {
+        this.#emitter.emit('token_update', set);
+        if (this.#config.oidcTokenSetFilePath) {
+            try {
+                await writeFile(this.#config.oidcTokenSetFilePath, JSON.stringify(set, null, 2));
+            } catch (err) {
+                this.#emitter.emit('error', 'Could not store OIDC tokenset to disk: ' + (err as Error).message);
+            }
         }
     };
 
 
-    private async _getTokenSet(): Promise<TokenSet> {
-        let token_set: TokenSet | null = this._token_set;
-        if (!token_set && (token_set = await this._loadTokenSet())){
-            this._token_set = token_set;
+    async #getTokenSet(): Promise<TokenSet> {
+        let tokenSet: TokenSet | null = this.#tokenSet;
+        if (!tokenSet && (tokenSet = await this.#loadTokenSet())){
+            this.#tokenSet = tokenSet;
         }
-        if (!token_set || !token_set.refresh_token) {
-            token_set = await this._authorize();
-        } else if (!token_set.expires_at || token_set.expires_at < (Date.now() / 1000) + 10) {
-            token_set = await this._refresh(token_set.refresh_token);
+        if (!tokenSet || !tokenSet.refresh_token) {
+            tokenSet = await this.#authorize();
+        } else if (!tokenSet.expires_at || tokenSet.expires_at < (Date.now() / 1000) + 10) {
+            tokenSet = await this.#refresh(tokenSet.refresh_token);
         }
-        if (this._token_set !== token_set) {
-            await this._storeTokenSet(token_set);
+        if (this.#tokenSet !== tokenSet) {
+            await this.#storeTokenSet(tokenSet);
         }
-        this._token_set = token_set;
-        return token_set;
+        this.#tokenSet = tokenSet;
+        return tokenSet;
     }
 
-    private async _getTokenSetQueued(): Promise<TokenSet> {
+    async #getTokenSetQueued(): Promise<TokenSet> {
         return new Promise((resolve, reject) => {
-            this._get_token_set_queue.push({ resolve, reject });
-            if (this._get_token_set_queue.length === 1) {
-                this._getTokenSet()
-                    .then((token_set) => {
-                        this._get_token_set_queue.forEach(({ resolve }) => resolve(token_set));
-                        this._get_token_set_queue = [];
+            this.#getTokenSetQueue.push({ resolve, reject });
+            if (this.#getTokenSetQueue.length === 1) {
+                this.#getTokenSet()
+                    .then((tokenSet) => {
+                        this.#getTokenSetQueue.forEach(({ resolve }) => resolve(tokenSet));
+                        this.#getTokenSetQueue = [];
                     })
                     .catch((err) => {
-                        this._get_token_set_queue.forEach(({ reject }) => reject(err));
-                        this._get_token_set_queue = [];
+                        this.#getTokenSetQueue.forEach(({ reject }) => reject(err));
+                        this.#getTokenSetQueue = [];
                     });
             }
         });
     }
 
     async requestResource(path: string, opts?: Parameters<typeof BaseClient.prototype.requestResource>[2]): Promise<any> {
-        const token_set = await this._getTokenSetQueued();
+        const tokenSet = await this.#getTokenSetQueued();
         const url = `${OnectaAPIBaseUrl.prod}${path}`;
-        const res = await this._client.requestResource(url, token_set, opts);
+        const res = await this.#client.requestResource(url, tokenSet, opts);
         if (res.body) {
             return JSON.parse(res.body.toString());
         }
